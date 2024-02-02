3ddf94f8be892e7b0b11c17d0b7e32340c99654a
Dante Benicio <dantecosta79@gmail.com>
2024-02-01 23:02:09 -0300

    Add project files.

---
+++ verificar-idade/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace verificar_idade
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Verificação de idade!");
+
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade > 0 && idade <= 10)
+            {
+                Console.WriteLine("Você é uma criança");
+            } else if (idade > 10 && idade <= 17)
+            {
+                Console.WriteLine("Você é um adolescente!");
+            } else if (idade >= 18 && idade < 60)
+            {
+                Console.WriteLine("Você é um adulto");
+            } else
+            {
+                Console.WriteLine("Você é um idoso!");
+            }
+        }
+    }
+}
